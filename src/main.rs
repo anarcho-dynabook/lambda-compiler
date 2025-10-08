ef15d1ec576580d14621ee0103743f5a04266996
Remilia Darknets <anarcho.dynabook@proton.me>
2025-10-08 16:08:56 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ impl Expr {
                 REGS[ctx.variable(name)?],
             )),
             Expr::Apply(la, arg) => Ok(format!(
-                "{}\tmov rbx, rax\n\tpush rbx\n{}\tpop rbx\n\tcall stackframe\t; Apply lambda: {la}\n",
+                "{}\tmov rbx, rax\n\tpush rbx\n{}\tpop rbx\n\tcall rax\t; Apply lambda: {la}\n",
                 arg.compile(ctx)?,
                 la.compile(ctx)?,
             )),
